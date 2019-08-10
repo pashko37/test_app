73222ac4cd87a207ba8a695049d2eecfbfd9f221
pashko37 <evobbs5@gmail.com>
2019-08-10 18:15:58 +0300

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'mypassword', password_confirmation: 'mypassword') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'mypassword', password_confirmation: 'mypassword')
